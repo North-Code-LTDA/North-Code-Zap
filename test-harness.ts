6e5d460e287fadf5f5160c5711f756c5e049ac34
North-Code-LTDA <northcode369@gmail.com>
2026-08-30 18:59:46 -0300

    chore: remove obsolete test files
    Delete test-esm.js, test-harness.js, and test-harness.ts as they are no longer required for the project.

--- test-harness.ts
+++
@@ -1,8 +0,0 @@
-import { InstanceManager } from './server/instances.ts';
-const manager = new InstanceManager();
-manager.reloadWorkspaceFromDisk('00000000-0000-0000-0000-000000000000')
-  .then(() => console.log('Successfully completed reloadWorkspaceFromDisk in ESM!'))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
